98f26a11d85b76e0a2cebddd8c72626a76de3d79
Adham Muhammad <adhamfarid1110@gmail.com>
2025-04-16 16:47:19 +0700

    fix: error ui in homepage

---
+++ src/pages/HomePage/components/PredictionForm.tsx
@@ -0,0 +1,66 @@
+import { Modal } from "antd";
+import { useState } from "react";
+
+const PredictionForm = () => {
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const handlePredictClick = () => {
+        setIsModalVisible(true);
+    };
+
+    const handleModalClose = () => {
+        setIsModalVisible(false);
+    };
+    return (
+        <>
+            <div className="label-row">
+                <label>Nominal Investasi</label>
+                <label>Tenor</label>
+            </div>
+
+            <div className="input-row">
+                <input
+                    type="text"
+                    placeholder="Input nominal yang ingin diinvestasikan"
+                />
+                <select>
+                    <option>Pilih Jangka Waktu</option>
+                    <option value="1">1 Bulan</option>
+                    <option value="3">3 Bulan</option>
+                    <option value="6">6 Bulan</option>
+                    <option value="12">12 Bulan</option>
+                </select>
+            </div>
+            <p
+                className="font-14 fw-300"
+                style={{ marginTop: "-10px", marginBottom: "20px" }}
+            >
+                Setara dengan <span className="fw-600">1.5 Gram</span> emas
+            </p>
+
+            <button className="btn-predict" onClick={handlePredictClick}>
+                Prediksi Keuntungan
+            </button>
+
+            <Modal
+                title="⚠️ Perhatian"
+                open={isModalVisible}
+                onCancel={handleModalClose}
+                onOk={handleModalClose}
+                okText="Lanjut"
+                cancelButtonProps={{ style: { display: "none" } }}
+            >
+                <p>
+                    Rekomendasi dari <strong>SAFEPICK</strong> bersifat{" "}
+                    <strong>prediksi</strong> dan tidak menjamin hasil pasti.
+                </p>
+                <p>
+                    Keputusan dan <strong>risiko</strong> investasi sepenuhnya
+                    menjadi <strong>tanggung jawab pengguna</strong>.
+                </p>
+            </Modal>
+        </>
+    );
+};
+
+export default PredictionForm;
